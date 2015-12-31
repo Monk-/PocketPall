329d21009a4b73fc1b1d0eed73c0935dbc4198fd
Aleksandr <monk_ak@yahoo.com>
2015-12-31 23:45:54 +0100

    splash screen

---
+++ app/src/main/java/com/example/user/pocketpall/SplashScreen.java
@@ -0,0 +1,32 @@
+package com.example.user.pocketpall;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 700);
+
+    }
+
+
+}
